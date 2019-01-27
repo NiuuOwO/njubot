7f045aca46721e0f166d80ed58208115ba7cd2be
NiuuOwO <patryk230206@gmail.com>
2019-01-27 18:01:10 +0100

    Update u-serwer.js

--- commands/u-serwer.js
+++ commands/u-serwer.js
@@ -14,6 +14,9 @@ module.exports.run = async (bot, message, args) => {
 
     message.channel.send(serverembed);
   }
+  if(message.content == "test"){
+    message.reply("test");
+  }
 }
 
 module.exports.help = {
