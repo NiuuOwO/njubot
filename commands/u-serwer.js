113a59ce29cb56f903628428568883cd007763fc
NiuuOwO <patryk230206@gmail.com>
2019-01-27 17:42:54 +0100

    Update u-serwer.js

--- commands/u-serwer.js
+++ commands/u-serwer.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
-  if(message.channel.id == "539022127643361310", "539022143564939264"){
+  if(message.channel.id == "539022127643361310"){
     let sicon = message.guild.iconURL;
     let serverembed = new Discord.RichEmbed()
     .setDescription("**Informacje o serwerze**")
