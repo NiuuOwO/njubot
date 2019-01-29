b9e1779cec8d0b7b2cfc5467dbf4b8df020eef73
NiuuOwO <patryk230206@gmail.com>
2019-01-29 16:08:31 +0100

    Update m-vote.js

--- commands/m-vote.js
+++ commands/m-vote.js
@@ -3,22 +3,18 @@ const Discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
   if(message.channel.id === "539022127643361310", "539022143564939264"){
     if(!message.member.hasPermission("MANAGE_CHANNELS")) return message.reply("DING DONG! Nie masz uprawnień aby użyć tą komendę. Pozdrawiam ;)");
-    let prop = message.guild.channels.find(`name`, "propozycje");
+    let prop = message.guild.channels.find(`name`, "ankiety");
     let main = message.guild.channels.find(`name`, "ogólny");
-    let kriejtedat = message.channel;
     if(!args[0]) return message.reply("Nie podano treści!");
-    let argsjeden = args.join(" ")
-    let argsdwa = args.join(" ")
 
     let propembed = new Discord.RichEmbed()
     .setColor("#0cf23d")
-    .setTitle(`**${argsjeden}**`)
-    .setDescription(argsdwa)
-    .addField("Autor:", message.author);
+    .setTitle(`**ANKIETA!**`)
+    .setDescription(args[0])
 
     let mainembed = new Discord.RichEmbed()
     .setColor("#0cf23d")
-    .addField("Nowa ankieta!", `Na kanale ${kriejtedat} pojawiła się nowa propozycja!`);
+    .addField("Nowa ankieta!", `Na kanale ${prop} pojawiła się nowa propozycja!`);
 
     message.reply("Wysłano propozycję!");
     main.send(mainembed);
